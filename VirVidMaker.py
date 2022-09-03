829f21c96f05814ff8585e4b0956de909245cf36
xemulat <98595166+xemulat@users.noreply.github.com>
2022-09-03 18:03:25 +0200

    Added updater and some checks

--- VirVidMaker.py
+++ VirVidMaker.py
@@ -3,6 +3,8 @@ from time import sleep
 from os import system, remove
 from sys import exit
 from urllib.request import urlretrieve
+from lastversion import has_update
+from ping3 import ping
 
 system('cls')
 sg.theme("DarkGray15")
@@ -11,6 +13,15 @@ urlretrieve('https://raw.githubusercontent.com/xemulat/VirusVideoMaker/main/text
 with open("fake-tmp.txt","r") as FileX:
     fpload = FileX.read()
 
+# ===============< The Injector >===============
+def injects(fname):
+    file1 = open(fname, "a")  # adds some fake malware text lul
+    file1.write('\n')
+    file1.write(fpload)
+    file1.close()
+    done()
+
+# ===============< Done Popup >===============
 def done():
     custom = [[sg.Text('Fake Virus has been injected')],
               [sg.Text('Click "X" to close!')],
@@ -25,19 +36,28 @@ def done():
             remove('fake-tmp.txt')
             exit()
 
-def injects(fname):
-    file1 = open(fname, "a")  # adds some fake malware text lul
-    file1.write('\n')
-    file1.write(fpload)
-    file1.close()
-    done()
-
 def getdata():
-    custom = [[sg.Text('Filename of the video to inject the "virus":')],
+    # ===============< Updater >===============
+    newver = has_update(repo='xemulat/VirusVideoMaker', current_version='1.1')
+    if "False" == newver:
+        vers = "Your Version is Up-To-Date!"
+    elif "True" == newver:
+        vers = "Your Version is Outdated, download it from My repo!"
+    else:
+        vers = "Unable to check updates :("
+    # ===============< Internet Chacker >===============
+    internet = ping("https://www.github.com/")
+    if internet == None or False:
+        internetac = "Network Unreachable"
+    else:
+        internetac = "Network Reachable"
+    # ===============< Main Window >===============
+    custom = [[sg.Text("Status: \n" + vers + "\n" + internetac)],
+              [sg.Text("")],
+              [sg.Text('Filename of the video to inject the "virus":')],
               [sg.Text('Press Enter to confirm')],
               [sg.Input('', enable_events=True,  key='-INPUT-', )],
-              [sg.Button('Exit')],
-              [sg.Button('Enter', visible=True, bind_return_key=True)],
+              [sg.Button('Exit'), sg.Button('Enter', visible=True, bind_return_key=True)],
               [sg.Text('')],
               [sg.Text('Coded by Xemulated')]]
 
@@ -46,9 +66,13 @@ def getdata():
     while True:
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
+            remove('fake-tmp.txt')
             exit()
         
         elif event == 'Enter':
-            window.close()
-            injects(window['-INPUT-'].get())
+            if window['-INPUT-'].get() == '':
+                sleep(0.001)
+            else:
+                window.close()
+                injects(window['-INPUT-'].get())
 getdata()
