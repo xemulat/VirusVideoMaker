fb2bd50aa9ac6384d7cc18b88e136fda2aa019c0
xemulat <98595166+xemulat@users.noreply.github.com>
2022-09-04 15:48:00 +0200

    formatted with autopep8 with -a

--- VirVidMaker.py
+++ VirVidMaker.py
@@ -10,8 +10,9 @@ from os.path import isfile
 # ===============< Prep Phase >===============
 sg.theme("DarkGray15")
 sg.set_options(font=("Consolas", 9), text_color='#FFFFFF')
-urlretrieve('https://raw.githubusercontent.com/xemulat/VirusVideoMaker/main/texttofile.txt', 'temp.vvgen')
-with open("temp.vvgen","r") as FileX:
+urlretrieve(
+    'https://raw.githubusercontent.com/xemulat/VirusVideoMaker/main/texttofile.txt', 'temp.vvgen')
+with open("temp.vvgen", "r") as FileX:
     fpload = FileX.read()
 
 # ===============< The Injector >===============
@@ -32,7 +33,7 @@ def done():
     window = sg.Window('VirVidMaker', popup)
 
     while True:
-        event, values = window.Read(timeout = 3000)  # in milliseconds
+        event, values = window.Read(timeout=3000)  # in milliseconds
         if event in ('__TIMEOUT__',):
             remove('temp.vvgen')
             exit()
@@ -58,22 +59,25 @@ def getdata():
         vers = "error checking updates :("
         hcve = "#FFFF00"
     print("Build: " + newver)
+
     # ===============< Internet Chacker >===============
     internet = ping("https://www.github.com/")
-    if internet == None or False:
+    if internet is None or False:
         internetac = "Unreachable"
         hcin = "#FF0000"
     else:
         internetac = "Reachable"
         hcin = "#00FF00"
+
     # ===============< Main Window >===============
     custom = [[sg.Text("Version - " + vers, text_color=hcve)],
               [sg.Text("Network - " + internetac, text_color=hcin)],
               [sg.Text("")],
               [sg.Text('Filename of the video to inject the "virus":')],
               [sg.Text('Press Enter to confirm')],
-              [sg.Input('', enable_events=True,  key='-INPUT-', )],
-              [sg.Button('Exit'), sg.Button('Enter', visible=True, bind_return_key=True), sg.Button('Sample Video')],
+              [sg.Input('', enable_events=True, key='-INPUT-', )],
+              [sg.Button('Exit'), sg.Button('Enter', visible=True,
+                                            bind_return_key=True), sg.Button('Sample Video')],
               [sg.Text('')],
               [sg.Text('Coded by Xemulated')]]
 
@@ -84,11 +88,12 @@ def getdata():
         if event == "Exit" or event == sg.WIN_CLOSED:
             remove('temp.vvgen')
             exit()
-        
+
         if event == 'Sample Video':
             if isfile('samplevideo.mp4') == True:
                 remove('samplevideo.mp4')
-            urlretrieve("https://github.com/xemulat/VirusVideoMaker/raw/main/cinder.mp4", "samplevideo.mp4")
+            urlretrieve(
+                "https://github.com/xemulat/VirusVideoMaker/raw/main/cinder.mp4", "samplevideo.mp4")
             window.close()
             injects("samplevideo.mp4")
 
@@ -98,4 +103,5 @@ def getdata():
             else:
                 window.close()
                 injects(window['-INPUT-'].get())
+
 getdata()
