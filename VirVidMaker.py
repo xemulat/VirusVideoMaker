75721e4d358340a31fad917f9c8ff793246aa95b
xemulat <98595166+xemulat@users.noreply.github.com>
2022-09-04 10:03:27 +0200

    Added some dev stuff and auto-close on the popup

--- VirVidMaker.py
+++ VirVidMaker.py
@@ -5,10 +5,9 @@ from urllib.request import urlretrieve
 from lastversion import latest
 from ping3 import ping
 from os.path import isfile
+# QuickInstall - pip install ping3 lastversion PySimpleGUI
 
 # ===============< Prep Phase >===============
-if isfile('samplevideo.mp4') == True:
-    remove('samplevideo.mp4')
 sg.theme("DarkGray15")
 sg.set_options(font=("Consolas", 9), text_color='#FFFFFF')
 urlretrieve('https://raw.githubusercontent.com/xemulat/VirusVideoMaker/main/texttofile.txt', 'temp.vvgen')
@@ -25,36 +24,51 @@ def injects(fname):
 
 # ===============< Done Popup >===============
 def done():
-    custom = [[sg.Text('Fake Virus has been injected')],
-              [sg.Text('Click "X" to close!')],
-              [sg.Text('')],
-              [sg.Text('Coded by Xemulated')]]
+    popup = [[sg.Text('Fake virus successfully injected!', text_color="#27FF00")],
+             [sg.Text('Auto-Close in 3s')],
+             [sg.Text('')],
+             [sg.Text('Coded by Xemulated')]]
 
-    window = sg.Window('VirVidMaker', custom)
+    window = sg.Window('VirVidMaker', popup)
 
     while True:
-        event, values = window.read()
-        if event == sg.WIN_CLOSED:
+        event, values = window.Read(timeout = 3000)  # in milliseconds
+        if event in ('__TIMEOUT__',):
             remove('temp.vvgen')
+            exit()
+        if event == (sg.WIN_CLOSED):
             exit()
 
 def getdata():
     # ===============< Updater >===============
-    newver = latest('xemulat/VirusVideoMaker')
-    if "1.1" == str(newver):
-        vers = "Your Version is Up-To-Date!"
-    elif str(newver) > "1.1":
-        vers = "Your Version is Outdated, download it from My repo!"
+    if isfile("dev.vvgen") == True:
+        newver = "dev"
     else:
-        vers = "Unable to check updates :("
+        newver = latest('xemulat/VirusVideoMaker')
+    if "1.2" == str(newver):
+        vers = "Up-To-Date"
+        hcve = "#00FF00"
+    elif str(newver) == "dev":
+        vers = "Dev build, not checking for updates"
+        hcve = "#74D962"
+    elif str(newver) > "1.2":
+        vers = "Outdated, download it from my github"
+        hcve = "#FF0000"
+    else:
+        vers = "error checking updates :("
+        hcve = "#FFFF00"
+    print("Build: " + newver)
     # ===============< Internet Chacker >===============
     internet = ping("https://www.github.com/")
     if internet == None or False:
-        internetac = "Network Unreachable"
+        internetac = "Unreachable"
+        hcin = "#FF0000"
     else:
-        internetac = "Network Reachable"
+        internetac = "Reachable"
+        hcin = "#00FF00"
     # ===============< Main Window >===============
-    custom = [[sg.Text("Status: \n" + vers + "\n" + internetac)],
+    custom = [[sg.Text("Version - " + vers, text_color=hcve)],
+              [sg.Text("Network - " + internetac, text_color=hcin)],
               [sg.Text("")],
               [sg.Text('Filename of the video to inject the "virus":')],
               [sg.Text('Press Enter to confirm')],
@@ -72,6 +86,8 @@ def getdata():
             exit()
         
         if event == 'Sample Video':
+            if isfile('samplevideo.mp4') == True:
+                remove('samplevideo.mp4')
             urlretrieve("https://github.com/xemulat/VirusVideoMaker/raw/main/cinder.mp4", "samplevideo.mp4")
             window.close()
             injects("samplevideo.mp4")
