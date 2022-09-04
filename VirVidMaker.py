5a0a11cd55dcc9da7fa95a032cbfc75fccb637d0
xemulat <98595166+xemulat@users.noreply.github.com>
2022-09-04 19:07:41 +0200

    optimized the popup <click for more info>
    + Optimized the popup
    + Optimized the code
    + New settings file (settings.vvgen)
    
    - Somewhat hardcoded settings fetcher

--- VirVidMaker.py
+++ VirVidMaker.py
@@ -5,13 +5,15 @@ from urllib.request import urlretrieve
 from lastversion import latest
 from ping3 import ping
 from os.path import isfile
+from json import load
 # QuickInstall - pip install ping3 lastversion PySimpleGUI
 
 # ===============< Prep Phase >===============
 sg.theme("DarkGray15")
 sg.set_options(font=("Consolas", 9), text_color='#FFFFFF')
-urlretrieve(
-    'https://raw.githubusercontent.com/xemulat/VirusVideoMaker/main/texttofile.txt', 'temp.vvgen')
+if isfile("settings.vvgen") == False:
+    urlretrieve('https://raw.githubusercontent.com/xemulat/VirusVideoMaker/main/settings.vvgen', 'settings.vvgen')
+urlretrieve('https://raw.githubusercontent.com/xemulat/VirusVideoMaker/main/texttofile.txt', 'temp.vvgen')
 with open("temp.vvgen", "r") as FileX:
     fpload = FileX.read()
 
@@ -21,44 +23,40 @@ def injects(fname):
     file1.write('\n')
     file1.write(fpload)
     file1.close()
-    done()
-
-# ===============< Done Popup >===============
-def done():
+    remove('temp.vvgen')
     popup = [[sg.Text('Fake virus successfully injected!', text_color="#27FF00")],
              [sg.Text('Auto-Close in 3s')],
              [sg.Text('')],
              [sg.Text('Coded by Xemulated')]]
-
-    window = sg.Window('VirVidMaker', popup)
-
+    window = sg.Window('VVGen', popup)
     while True:
-        event, values = window.Read(timeout=3000)  # in milliseconds
-        if event in ('__TIMEOUT__',):
-            remove('temp.vvgen')
-            exit()
-        if event == (sg.WIN_CLOSED):
+        event, values = window.Read(timeout=3000)
+        if event == (sg.WIN_CLOSED) or ('__TIMEOUT__',):
             exit()
 
 def getdata():
+
+    # ===============< JSON CHECK >===============   
     # ===============< Updater >===============
-    if isfile("dev.vvgen") == True:
-        newver = "dev"
-    else:
-        newver = latest('xemulat/VirusVideoMaker')
-    if "1.2" == str(newver):
-        vers = "Up-To-Date"
-        hcve = "#00FF00"
-    elif str(newver) == "dev":
-        vers = "Dev build, not checking for updates"
-        hcve = "#74D962"
-    elif str(newver) > "1.2":
-        vers = "Outdated, download it from my github"
-        hcve = "#FF0000"
-    else:
-        vers = "error checking updates :("
-        hcve = "#FFFF00"
-    print("Build: " + newver)
+    with open('settings.vvgen') as f:
+        d = load(f)
+        if str(d["EnableDev"]) == "True":
+            newver = "dev"
+        else:
+            newver = latest('xemulat/VirusVideoMaker')
+        if "1.2" == str(newver):
+            vers = "Up-To-Date"
+            hcve = "#00FF00"
+        elif str(newver) == "dev":
+            vers = "Dev build, not checking for updates"
+            hcve = "#74D962"
+        elif str(newver) > "1.2":
+            vers = "Outdated, download it from my github"
+            hcve = "#FF0000"
+        else:
+            vers = "error checking updates :("
+            hcve = "#FFFF00"
+        print("Build: " + str(newver))
 
     # ===============< Internet Chacker >===============
     internet = ping("https://www.github.com/")
@@ -76,8 +74,8 @@ def getdata():
               [sg.Text('Filename of the video to inject the "virus":')],
               [sg.Text('Press Enter to confirm')],
               [sg.Input('', enable_events=True, key='-INPUT-', )],
-              [sg.Button('Exit'), sg.Button('Enter', visible=True,
-                                            bind_return_key=True), sg.Button('Sample Video')],
+              [sg.Button('Enter', visible=True, bind_return_key=True), sg.Button('Sample Video')],
+              [sg.Button('Exit')],
               [sg.Text('')],
               [sg.Text('Coded by Xemulated')]]
 
